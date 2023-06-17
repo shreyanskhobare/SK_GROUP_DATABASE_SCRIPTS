e2c0d1146c9308bb41d4da41044451035ca548ab
shreyu <shreyu@DESKTOP-5R1VB7D.Home>
2023-06-17 16:12:12 +0530

    First Commit! Scripts for basic setup

---
+++ CREATE_INSERT/INSERT_VALUES_FOR_TESTING.sql
@@ -0,0 +1,6 @@
+INSERT INTO EMPLOYEE VALUES (null, 'Mr', 'Joey', 'Tribbiani',true, 'joey.tribbiani@organization.com');
+COMMIT;
+
+INSERT INTO EMPLOYEE_PERSONAL_INFO VALUES (1, '+447444444444', '02022222222','joey.trib@gmail.com', 'Croydon','Croydon', '2023-02-02', null );
+COMMIT;
+

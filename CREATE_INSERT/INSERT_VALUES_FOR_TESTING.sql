8cf10ba3694e464d2d9407849cb271c888893315
shreyanskhobare <83969833+shreyanskhobare@users.noreply.github.com>
2023-06-22 02:05:29 +0530

    Added Unique Keyword as per requirement.

--- CREATE_INSERT/INSERT_VALUES_FOR_TESTING.sql
+++ CREATE_INSERT/INSERT_VALUES_FOR_TESTING.sql
@@ -1,6 +1,6 @@
-INSERT INTO EMPLOYEE VALUES (null, 'Mr', 'Joey', 'Tribbiani',true, 'joey.tribbiani@organization.com');
+INSERT INTO EMPLOYEE (TITLE,FIRST_NAME,LAST_NAME,EMPLOYMENT_ACTIVE,ORGANIZATION_EMAIL) VALUES ('Mr', 'Joey', 'Tribbiani',true, 'joey.tribbiani@organization.com');
 COMMIT;
 
-INSERT INTO EMPLOYEE_PERSONAL_INFO VALUES (1, '+447444444444', '02022222222','joey.trib@gmail.com', 'Croydon','Croydon', '2023-02-02', null );
+INSERT INTO EMPLOYEE_PERSONAL_INFO(EMPLOYEE_ID,MOBILE_NUMBER,PERSONAL_EMAIL,ADDRESS,JOINING_DATE,LEAVING_DATE) VALUES (1, '+447444444444', 'joey.trib@gmail.com', 'Croydon', '2023-02-02', null );
 COMMIT;
 
